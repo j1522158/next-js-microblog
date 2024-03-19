cacbddf5df5d48f33bd3d6e5d6040bc0bc18c01d
j1522158 <j1522158vu@gmail.com>
2024-03-20 00:35:00 +0900

    "getStaticPaths, getStaticPropsの実装"

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,33 @@
 import Layout from "../../components/ Layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
 
-export default function Post() {
-    return <Layout>動的ルーティング</Layout>;
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+
+    return {
+        paths,
+        fallback: false,  // 上記のpathsに含まれていないページは404 NotFoundとなる
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default function Post({ postData }) {
+    return (
+    <Layout>
+        {postData.title}
+        <br />
+        {postData.date}
+        <br />
+        {postData.blogContentHTML}
+    </Layout>
+    );
 }
